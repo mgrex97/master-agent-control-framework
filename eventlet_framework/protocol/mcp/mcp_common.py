72a42477784dd78327c391ba9390d0b8a362d562
carlton lin <carltonlin@qnap.com>
2022-05-18 14:34:28 +0800

    Ammend MCP_HEADER_PACK_STR.

--- eventlet_framework/protocol/mcp/mcp_common.py
+++ eventlet_framework/protocol/mcp/mcp_common.py
@@ -1,7 +1,7 @@
 from struct import calcsize
 
 # Machine Service Header
-MCP_HEADER_PACK_STR = '!BHHHBI'
+MCP_HEADER_PACK_STR = '!HHII'
 MCP_HEADER_SIZE = 12
 
 assert calcsize(MCP_HEADER_PACK_STR) == MCP_HEADER_SIZE
