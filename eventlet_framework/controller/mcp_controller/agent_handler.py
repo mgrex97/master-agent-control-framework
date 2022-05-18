4e738cc70c9a86ee221b2b18741beea10314eae8
carlton lin <carltonlin@qnap.com>
2022-05-18 15:23:55 +0800

    Create MCP agent controller and handler.

---
+++ eventlet_framework/controller/mcp_controller/agent_handler.py
@@ -0,0 +1,28 @@
+from eventlet_framework.base.app_manager import BaseApp
+from eventlet_framework.lib import hub
+from eventlet_framework.event import event
+from eventlet_framework.event.mcp_event import mcp_event
+from eventlet_framework.controller.handler import observe_event
+from eventlet_framework.controller.mcp_controller.mcp_state import MC_HANDSHAK
+from eventlet_framework.controller.mcp_controller.agent_controller import AgentConnection, MachineControlAgentController
+
+
+class AgentMCPHandler(BaseApp):
+    def __init__(self, *_args, **_kwargs):
+        super().__init__(*_args, **_kwargs)
+        self.name = 'mcp_agent_handler'
+        self.controller = None
+
+    def start(self):
+        super().start()
+        self.controller = MachineControlAgentController()
+        return hub.spawn(self.controller)
+
+    @observe_event(event.EventSocketConnecting, MC_HANDSHAK)
+    def connecting_handler(self, ev: event.EventSocketConnecting):
+        pass
+
+    @observe_event(mcp_event.EventMCPHello, MC_HANDSHAK)
+    def hello_handler(self, ev):
+        print('get hello msg')
+        pass
