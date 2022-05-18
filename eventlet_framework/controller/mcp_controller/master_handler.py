cce9a8f4ef4f85d2b444ba10ae81867975669020
carlton lin <carltonlin@qnap.com>
2022-05-18 15:07:13 +0800

    Create master_handler.py.

--- eventlet_framework/controller/mcp_controller/master_handler.py
+++ eventlet_framework/controller/mcp_controller/master_handler.py
@@ -1,6 +1,6 @@
 from eventlet_framework.controller.handler import observe_event
 from eventlet_framework.base.app_manager import BaseApp
-from eventlet_framework.controller.mcp_controller.mcp_controller import MachineControlMasterController
+from eventlet_framework.controller.mcp_controller.master_controller import MachineControlMasterController
 from eventlet_framework.lib import hub
 from eventlet_framework.event.mcp_event import mcp_event
 from eventlet_framework.controller.mcp_controller.mcp_state import MC_HANDSHAK
@@ -9,7 +9,7 @@ from eventlet_framework.controller.mcp_controller.mcp_state import MC_HANDSHAK
 class MCPMasterHandler(BaseApp):
     def __init__(self, *_args, **_kwargs):
         super(MCPMasterHandler, self).__init__(*_args, **_kwargs)
-        self.name = 'mcp_handler'
+        self.name = 'mcp_master_handler'
         self.controller = None
 
     def start(self):
