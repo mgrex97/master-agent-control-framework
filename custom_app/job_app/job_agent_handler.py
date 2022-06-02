d55677728e6bc5a6bacfcd9cec65af61db3a5a53
carlton lin <carltonlin@qnap.com>
2022-06-02 16:10:16 +0800

    Amend JobAgentHandler. Finish job create and ack procedure.

--- custom_app/job_app/job_agent_handler.py
+++ custom_app/job_app/job_agent_handler.py
@@ -1,11 +1,16 @@
+import logging
 from custom_app.job_app.job_class import JOB_ASYNC, Job
-from eventlet_framework.protocol.mcp.mcp_parser_v_1_0 import MCPJobCreateReply
 from eventlet_framework.base.app_manager import BaseApp
 from eventlet_framework.event import event
 from eventlet_framework.event.mcp_event import mcp_event
-from eventlet_framework.controller.handler import observe_event, observe_event_without_event_source
-from eventlet_framework.controller.mcp_controller.mcp_state import MC_HANDSHAK
+from eventlet_framework.controller.handler import observe_event
+from eventlet_framework.controller.mcp_controller.mcp_state import MC_STABLE
 from custom_app.job_app.job_manager import JobManager
+
+_REQUIRED_APP = [
+    'eventlet_framework.controller.mcp_controller.agent_handler']
+
+LOG = logging.getLogger('custom_app.job_app.jog_agent_handler')
 
 
 class JobAgentHandler(BaseApp):
@@ -13,30 +18,37 @@ class JobAgentHandler(BaseApp):
         super().__init__(*_args, **_kwargs)
         self.name = 'job_agent_handler'
         self.job_manager: JobManager = None
+        self.connection = None
 
-    @observe_event(event.EventSocketConnecting, MC_HANDSHAK)
+    @observe_event(mcp_event.EventMCPStateChange, MC_STABLE)
     def connect_to_master(self, ev):
         if self.job_manager is not None:
             del self.job_manager
-        self.job_manager = JobManager(ev.connection)
 
-    @observe_event(mcp_event.EventMCPJobCreateRequest, MC_HANDSHAK)
+        self.job_manager = JobManager(ev.connection)
+        self.connection = ev.connection
+
+    @observe_event(mcp_event.EventMCPJobCreateRequest, MC_STABLE)
     def job_create_request_handler(self, ev):
-        conn = ev.connection
+        LOG.info(f"Get job create request from {self.connection.address}")
+        conn = self.connection
         xid = ev.msg.xid
 
         new_job_id = self.job_manager.get_new_job_id()
-        job_obj = Job.create_job_by_job_info(ev.msg.job_info, new_job_id)
+        job_obj = Job.create_job_by_job_info(
+            self.connection, ev.msg.job_info, new_job_id)
 
         self.job_manager.add_job(job_obj)
 
+        LOG.info(f"Job Info:{ev.msg.job_info}")
+
         msg = conn.mcproto_parser.MCPJobCreateReply(
             conn, new_job_id, job_info=job_obj.job_info_serialize())
-        msg.set_xid(xid)
+        msg.xid = xid
         conn.send_msg(msg)
 
-    @observe_event(mcp_event.EventMCPJobACK, MC_HANDSHAK)
-    def master_job_ack_handler(self, ev):
-        job_obj: Job = self.job_manager.get_job(ev.msg.job_id)
-        job_obj.change_state(JOB_ASYNC)
-        job_obj.run_job()
+    @observe_event(mcp_event.EventMCPJobACK, MC_STABLE)
+    def job_ack_hanlder(self, ev):
+        job: Job = self.job_manager.get_job(ev.msg.job_id)
+        job.change_state(JOB_ASYNC)
+        job.run_job()
--- custom_app/job_app/job_master_handler.py
+++ custom_app/job_app/job_master_handler.py
@@ -1,10 +1,15 @@
-from custom_app.job_app.job_class import JOB_ASYNC, JobCommand, Job
+import logging
+from custom_app.job_app.job_class import JOB_ASYNC, JobCommand
 from eventlet_framework.base.app_manager import BaseApp
 from eventlet_framework.event import event
 from eventlet_framework.event.mcp_event import mcp_event
-from eventlet_framework.controller.handler import observe_event, observe_event_without_event_source
-from eventlet_framework.controller.mcp_controller.mcp_state import MC_HANDSHAK
+from eventlet_framework.controller.handler import observe_event
+from eventlet_framework.controller.mcp_controller.mcp_state import MC_STABLE
 from custom_app.job_app.job_manager import JobManager
+
+_REQUIRED_APP = ['eventlet_framework.controller.mcp_controller.master_handler']
+
+LOG = logging.getLogger('custom_app.job_app.jog_master_handler')
 
 
 class JobMasterHandler(BaseApp):
@@ -13,36 +18,45 @@ class JobMasterHandler(BaseApp):
         self.name = 'job_master_handler'
         self.job_managers = {}
 
-    @observe_event(event.EventSocketConnecting, MC_HANDSHAK)
+    @observe_event(mcp_event.EventMCPStateChange, MC_STABLE)
     def agent_join(self, ev):
-        self.job_managers[ev.connection.address] = JobManager(
+        conn = ev.connection
+        self.job_managers[conn.id] = JobManager(
             ev.connection)
 
     def agent_machine_leave(self, ev):
         del self.job_managers[ev.connection.address]
 
-    @observe_event(mcp_event.EventMCPJobCreateReply, MC_HANDSHAK)
+    @observe_event(mcp_event.EventMCPJobCreateReply, MC_STABLE)
     def job_create_reply_handler(self, ev):
-        msg = ev.msg
-        conn = ev.connection
-        job_id = ev.msg.job_id
-        self.job_managers[conn.address].job_id_async(msg.xid, msg.job_id)
+        LOG.info(f'Get Job create reply')
 
-        msg = conn.mcproto_parser.MCPCommandACK(conn, job_id)
+        msg = ev.msg
+        conn = ev.msg.connection
+        job_id = ev.msg.job_id
+        self.job_managers[conn.id].job_id_async(msg.xid, msg.job_id)
+
+        msg = conn.mcproto_parser.MCPJobACK(conn, job_id)
         conn.send_msg(msg)
 
-    @observe_event(mcp_event.EventMCPJobStateInform, MC_HANDSHAK)
+    @observe_event(mcp_event.EventMCPJobStateInform, MC_STABLE)
     def job_state_inform_handler(self, ev):
-        print('get inform')
+        LOG.info('get inform')
+        pass
+
+    def job_delete(self, connection_id, job_id):
+        self.job_managers[connection_id].get_job(job_id)
 
     def exe_cmd_on_agent(self, address, command):
-        assert address not in self.job_managers
+        assert address in self.job_managers
 
         job_manager: JobManager = self.job_managers[address]
         job = JobCommand(job_manager.connection, command)
 
-        msg = job_manager.connection.mcproto_parser.MCPCommandExecuteRequest(
+        msg = job_manager.connection.mcproto_parser.MCPJobCreateRequest(
             job_manager.connection, timeout=10, job_info=job.job_info_serialize())
-        job_manager.connection.send_msg(msg)
 
+        # msg's xid is None, give new xid to msg.
+        job_manager.connection.set_xid(msg)
         job_manager.add_request(xid=msg.xid, job_obj=job)
+        job_manager.connection.send_msg(msg)
