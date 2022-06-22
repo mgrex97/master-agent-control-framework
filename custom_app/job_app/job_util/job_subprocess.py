d68a63e20b2e314fbd10ea0bae8e66a1b7dc5759
carlton lin <carltonlin@qnap.com>
2022-06-22 13:31:37 +0800

    Adapte new mechanism into job_subprocess.py.

--- custom_app/job_app/job_util/job_subprocess.py
+++ custom_app/job_app/job_util/job_subprocess.py
@@ -2,11 +2,10 @@ import logging
 import json
 import asyncio
 import traceback
+
 from async_app_fw.lib import hub
-from async_app_fw.base.app_manager import lookup_service_brick
 from async_app_fw.controller.mcp_controller.mcp_controller import MachineConnection
-from custom_app.job_app.job_agent_handler import APP_NAME_JobAgentHandler
-from job_class import Job
+from custom_app.job_app.job_class import Job, JOB_RUN, JOB_RUNNING, JOB_STOP, JOB_STOPED, JOB_STOPING, action_handler, collect_handler, handle_state_change, observe_output
 
 CMD_JOB = 1
 
@@ -19,15 +18,15 @@ class ProcessStillRunning(Exception):
     pass
 
 
-LOG = logging.getLogger('Job Command')
+LOG = logging.getLogger('Job Process Command')
 
 
+@collect_handler
 @Job.register_job_type(CMD_JOB)
 class JobCommand(Job):
     def __init__(self, command, connection=None, timeout=60, state_inform_interval=5):
         super().__init__(connection, timeout, state_inform_interval)
         self.command = command
-        self.command_running_task = None
         self.__process = None
         self.stdin = None
         self.stdout = None
@@ -49,61 +48,42 @@ class JobCommand(Job):
         }
         return output
 
-    def run_job(self):
-        super().run_job()
-        self.run_command()
+    @observe_output(JOB_RUNNING)
+    def get_output(state, output):
+        print(output)
 
-    def run_command(self, auto_stop=False):
-        async def _run_command():
-            try:
-                self.__process = await asyncio.create_subprocess_shell(
-                    self.command,
-                    stdout=asyncio.subprocess.PIPE,
-                    stderr=asyncio.subprocess.PIPE
-                )
+    @action_handler(JOB_RUN, JOB_RUNNING)
+    def run(self):
+        pass
 
-                self.stdin = self.__process.stdin
-                self.stdout = self.__process.stdout
-                self.stderr = self.__process.stderr
-                self.state = True
-                self.read_stdout()
-                # wait std read task end
-                task_loop = hub.TaskLoop(
-                    hub.app_hub, list(self.std_task.values()))
-                await task_loop.wait_tasks()
-            except asyncio.CancelledError:
-                pass
-            finally:
-                self.stop()
-                self.command_running_task = None
+    @handle_state_change((JOB_RUN, JOB_RUNNING), JOB_STOP)
+    async def _run_command(self):
+        try:
+            self.__process = await asyncio.create_subprocess_shell(
+                self.command,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE
+            )
 
-        if self.command_running_task is None:
+            self.stdin = self.__process.stdin
+            self.stdout = self.__process.stdout
+            self.stderr = self.__process.stderr
+            self.state = True
+            self.read_stdout()
+            # wait std read task end
+            task_loop = hub.TaskLoop(
+                hub.app_hub, list(self.std_task.values()))
+            await task_loop.wait_tasks()
+        except asyncio.CancelledError:
             pass
-        elif not self.command_running_task.done():
-            if auto_stop is True:
-                self.command_running_task.cancel()
-            else:
-                raise ProcessStillRunning()
 
-        self.command_running_task = hub.app_hub.spawn(_run_command)
-        return self.command_running_task
+        self.LOG.info(f'Command {self.command} running end.')
 
     def read_stdout(self):
         self.read_output_from_std(self.stdout, 'out')
 
     def read_stderr(self):
         self.read_output_from_std(self.stderr, 'err')
-
-    """
-    def output_handler(self, output_info):
-        # prevent queue from full.
-        if self.output_queue.full():
-            self.output_queue.get()
-
-        self.output_queue.put(output_info)
-        LOG.warning(
-            f'Get CMD output from {self.connection.address}, Output: {output_info}')
-    """
 
     def read_output_from_std(self, std, type):
         if std is None:
@@ -114,18 +94,19 @@ class JobCommand(Job):
                 while self.state and self.__process:
                     # get = await std.readline()
                     get = await std.readline()
-                    print(get)
                     if len(get) == 0:
                         break
                     output_info = super(JobCommand, self).job_info_serialize()
                     output_info['output'] = get.decode(encoding='utf-8')
 
-                    output_msg = self.connection.mcproto_parser.MCPJobOutput(
-                        self.connection, self.id, json.dumps(output_info))
-
-                    self.connection.send_msg(output_msg)
+                    self.get_output(get)
+                    # output_msg = self.connection.mcproto_parser.MCPJobOutput(
+                    # self.connection, self.id, json.dumps(output_info))
+                    # self.connection.send_msg(output_msg)
             except asyncio.CancelledError:
                 pass
+            except Exception:
+                print(traceback.format_exc())
 
         # if task not exist yet.
         if (task := self.std_task.get(type, None)) is None:
@@ -138,41 +119,33 @@ class JobCommand(Job):
 
         self.std_task.setdefault(type, task)
 
+    @action_handler(JOB_STOP, JOB_STOPING)
     def stop(self):
-        super().stop()
-
-        async def _subprocess_stop(process):
-            # make sure all std tasks are canceled.
-            if isinstance(process, asyncio.subprocess.Process):
-                try:
-                    process.kill()
-                except ProcessLookupError:
-                    pass
-                    # LOG.warning(
-                    #    f'Process Kill Exception, {traceback.format_exc()}')
-                except Exception:
-                    LOG.warning(
-                        f'Process Kill Exception, {traceback.format_exc()}')
-
-            std_type = list(self.std_task.keys())
-            for k in std_type:
-                std_task = self.std_task.pop(k)
-                if not std_task.done():
-                    std_task.cancel()
-
-        process = self.__process
-        self.__process = None
         self.stdin = None
         self.stderr = None
         self.stdout = None
-        self.connection = None
 
-        # if there is std task exist.
-        if self.std_task:
-            return hub.app_hub.spawn(_subprocess_stop, process)
-        else:
-            None
+    @handle_state_change((JOB_STOP, JOB_STOPING), JOB_STOPED)
+    async def _subprocess_stop(self):
+        process = self.__process
+        self.__process = None
+        # make sure all std tasks are canceled.
+        if isinstance(process, asyncio.subprocess.Process):
+            try:
+                process.kill()
+            except ProcessLookupError:
+                pass
+                # LOG.warning(
+                #    f'Process Kill Exception, {traceback.format_exc()}')
+            except Exception:
+                LOG.warning(
+                    f'Process Kill Exception, {traceback.format_exc()}')
+
+        std_type = list(self.std_task.keys())
+        for k in std_type:
+            std_task = self.std_task.pop(k)
+            if not std_task.done():
+                std_task.cancel()
 
     def __del__(self):
-        if self.command_running_task:
-            self.command_running_task.cancel()
+        self.stop()
