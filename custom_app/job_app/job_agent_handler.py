3a952d104d78fc21435727d56a8bf2e133a73c8f
carlton lin <carltonlin@qnap.com>
2022-06-22 23:23:06 +0800

    Adpate remote machanisim into job_agent_handler.py.

--- custom_app/job_app/job_agent_handler.py
+++ custom_app/job_app/job_agent_handler.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from custom_app.job_app.job_util.job_class import Job
+from custom_app.job_app.job_util.job_class import REMOTE_AGENT, Job
 from async_app_fw.base.app_manager import BaseApp
 from async_app_fw.event import event
 from async_app_fw.event.mcp_event import mcp_event
@@ -8,7 +8,6 @@ from async_app_fw.controller.handler import observe_event
 from async_app_fw.controller.mcp_controller.mcp_state import MC_DISCONNECT, MC_STABLE
 from async_app_fw.protocol.mcp.mcp_parser_v_1_0 import MCPJobStateChange
 from custom_app.job_app.job_manager import JobManager
-from async_app_fw.lib.hub import TaskLoop, app_hub
 
 _REQUIRED_APP = [
     'async_app_fw.controller.mcp_controller.agent_handler']
@@ -45,7 +44,7 @@ class JobAgentHandler(BaseApp):
 
         new_job_id = self.job_manager.get_new_job_id()
         job_obj = Job.create_job_by_job_info(
-            self.connection, ev.msg.job_info, new_job_id)
+            self.connection, ev.msg.job_info, new_job_id, REMOTE_AGENT)
 
         self.job_manager.add_job(job_obj)
 
@@ -61,16 +60,12 @@ class JobAgentHandler(BaseApp):
     def job_state_change_hanlder(self, ev):
         msg: MCPJobStateChange = ev.msg
         job: Job = self.job_manager.get_job(ev.msg.job_id)
-        job.change_state(msg.before, msg.after, msg.info)
+        job.remote_change_state(msg.before, msg.after, msg.info)
 
     @observe_event(mcp_event.EventMCPJobDeleteRequest, MC_STABLE)
     def job_delete_request_hanlder(self, ev):
         self.LOG.info(f"Delete Job request, Job ID <{ev.msg.job_id}>")
         self.job_manager.del_job(ev.msg.job_id)
-        # conn = ev.connection
-        # reply delete result
-        # reply_msg = ev.msg.cls_msg_type(ev.connection, ev.msg.job_id)
-        # conn.set_xid(reply_msg)
 
     @observe_event(mcp_event.EventMCPJobDeleteAll, MC_STABLE)
     def job_delete_all_handler(self, ev):
