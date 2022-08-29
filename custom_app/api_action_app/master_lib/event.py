1f0a33ae3850df985e03c26cc21c4595f137016c
carltonlin <carltonlin@qnap.com>
2022-08-29 18:48:05 +0800

    Amend api_action_app.master_lib.event .

--- custom_app/api_action_app/master_lib/event.py
+++ custom_app/api_action_app/master_lib/event.py
@@ -1,30 +1,25 @@
 from ..constant import API_ACTION_CONTROLLER_MASTER_APP_NAME as APP_NAME, AGENT_LOCAL\
-    , DEFAULT_SESSION_INIT_TIMEOUT, DEFAULT_LOGIN_TIMEOUT
-from custom_app.util.async_api_action import SessionInfo
+    , DEFAULT_LOGIN_TIMEOUT
+from custom_app.util.async_api_action import APIAction, SessionInfo
 from async_app_fw.event.async_event import EventAsyncRequestBase
 
-class ReqSessionInit(EventAsyncRequestBase):
-    REQUEST_NAME = 'API Session Init'
+class ReqRemoteAPILogin(EventAsyncRequestBase):
+    REQUEST_NAME = 'API login'
     DST_NAME = APP_NAME
 
-    def __init__(self, api_hostname, base_url, login_info=None, auth=None, timeout=DEFAULT_SESSION_INIT_TIMEOUT):
-        super().__init__(timeout)
-        self.session_info = SessionInfo(
-            api_hostname,
-            base_url,
-            login_info,
-            auth
-        )
-
-class ReqAPILoginCheck(EventAsyncRequestBase):
-    REQUEST_NAME = 'API login check'
-    DST_NAME = APP_NAME
-
-    def __init__(self, api_hostname, session_info=None, agent_address=AGENT_LOCAL, timeout=DEFAULT_LOGIN_TIMEOUT):
+    def __init__(self, api_action, *args, agent=AGENT_LOCAL, timeout=DEFAULT_LOGIN_TIMEOUT, **kwargs):
         super().__init__(timeout=timeout)
-        self.api_hostname = api_hostname
-        self.session_info = session_info
-        self.agent_address = agent_address
+
+        if not isinstance(api_action, APIAction):
+            self.push_reply(TypeError('Input Variable is not instance of APIAction (async ver).'))
+            return
+
+        self.api_action = api_action
+        self.timeout = timeout
+        self.session_info = api_action.get_session_info()
+        self.agent = agent
+        self.args = args
+        self.kwargs = kwargs
 
 class ReqSendRequestFromRemote(EventAsyncRequestBase):
     REQUEST_NAME = 'Rquest from remote agent.'
@@ -37,11 +32,3 @@ class ReqSendRequestFromRemote(EventAsyncRequestBase):
         self.api_action = api_action
         self.args = args
         self.kwargs = kwargs
-
-class ReqGetAPIAction(EventAsyncRequestBase):
-    REQUEST_NAME = 'Get API Action'
-    DST_NAME = APP_NAME
-
-    def __init__(self, api_hostname, timeout=None):
-        super().__init__(timeout=timeout)
-        self.api_hostname = api_hostname
