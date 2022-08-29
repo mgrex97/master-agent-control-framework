ff25bca7fde7e7c558da39d1134ade3ab12f1153
carltonlin <carltonlin@qnap.com>
2022-08-29 18:48:28 +0800

    Amend api_action_app.master_lib.util .

--- custom_app/api_action_app/master_lib/util.py
+++ custom_app/api_action_app/master_lib/util.py
@@ -1,21 +1,19 @@
-from ..constant import AGENT_LOCAL, REMOTE_REQUEST_DEFAULT_TIMEOUT
+from ..constant import AGENT_LOCAL, REMOTE_REQUEST_DEFAULT_TIMEOUT, API_ACTION_CONTROLLER_MASTER_APP_NAME as APP_NAME
+from async_app_fw.base.app_manager import lookup_service_brick
 from custom_app.util.async_api_action import APIAction
-from .event import ReqSendRequestFromRemote
+from .event import ReqSendRequestFromRemote, ReqRemoteAPILogin
 
 # This decorator make request methods(post,get...) have ability to execute request on agent.
 def remote_request_decorator(fun):
     method = getattr(fun, '_method_name')
 
     async def _remote_send_request(self, *args, agent=None, timeout=REMOTE_REQUEST_DEFAULT_TIMEOUT,method_name=method, **kwargs):
-        if getattr(self, 'default_agent', None) is None:
-            self.default_agent = AGENT_LOCAL
-
         agent = agent or self.default_agent
 
         if agent == AGENT_LOCAL:
-            res = await fun(self, *args, *kwargs)
+            res = await fun(self, *args, **kwargs)
         else:
-            # send request (Event) to APIActionMasterController to deal with remote request.
+            # send event to APIActionMasterController to deal with remote request.
             res = await ReqSendRequestFromRemote.send_request(agent, method_name, self, *args, timeout=timeout, **kwargs)
 
             if (callback := (kwargs.get('callback', None))) is not None:
@@ -27,15 +25,36 @@ def remote_request_decorator(fun):
 
     return _remote_send_request
 
-def remote_request_init_decorator(cls: APIAction):
+def api_action_init_decorator(cls: APIAction):
     init_method = cls.__init__
 
     def __init__(self: APIAction, *args, default_agent=AGENT_LOCAL, **kwargs):
         init_method(self, *args, **kwargs)
         self.default_agent = default_agent
+        api_action_controller = lookup_service_brick(APP_NAME)
+        api_action_controller.register_api_action(self)
  
     def set_default_agent(self, default_agent):
         self.default_agent = default_agent
  
-    cls.__init__ = init_method
+    cls.__init__ = __init__
     cls.set_default_agent = set_default_agent
+
+def remote_login_decorator(cls: APIAction):
+    login_method = cls.login_api
+
+    async def login_api(self, *args, agent=None, **kwargs):
+        agent = agent or self.default_agent
+
+        if agent == AGENT_LOCAL:
+            res = await login_method(self, *args, **kwargs)
+        else:
+            # Send event to APIActionMasterController to deal with remote login.
+            res = await ReqRemoteAPILogin.send_request(self, agent=agent, *args , **kwargs)
+
+            if (callback := (kwargs.get('callback', None))) is not None:
+                callback(res)
+        
+        return res
+ 
+    cls.login_api = login_api
