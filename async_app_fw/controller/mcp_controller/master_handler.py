16137afdb561f08fa358109659afbc359478c81e
carltonlin <carltonlin@qnap.com>
2022-09-21 11:12:09 +0800

    Add connection waiting procedure into master_handler.py.

--- async_app_fw/controller/mcp_controller/master_handler.py
+++ async_app_fw/controller/mcp_controller/master_handler.py
@@ -1,6 +1,10 @@
+import asyncio
+from collections import defaultdict
 import logging
+from typing import Dict, Set
 from async_app_fw.controller.handler import observe_event, observe_event_from_self
 from async_app_fw.base.app_manager import BaseApp
+from async_app_fw.lib.hub import app_hub
 from async_app_fw.event import event
 from async_app_fw.controller.mcp_controller.mcp_state import \
     MC_DISCONNECT, MC_FEATURE, MC_HANDSHAK, MC_STABLE
@@ -8,8 +12,10 @@ from async_app_fw.controller.mcp_controller.master_controller import MachineCont
 from async_app_fw.event.mcp_event import mcp_event
 
 from .master_lib.constant import APP_NAME
-from .master_lib.event import ReqGetAgentConnection
+from .master_lib.event import ReqGetAgentConnection, ReqWaitAgentsConnect
 from .exception import AgentIsNotExist
+
+spawn = app_hub.spawn
 
 LOG = logging.getLogger(
     'async_app_fw.controller.mcp_controller.agent_controller')
@@ -26,6 +32,8 @@ class MCPMasterHandler(BaseApp):
         self.controller = None
         self.connection_dict = {}
         self.ip_to_connection = {}
+        self.connection_event: Dict[str, asyncio.Event] = defaultdict(lambda: asyncio.Event())
+        self.connection_check_task: Set[asyncio.Task] = set()
 
     def start(self):
         task = super(MCPMasterHandler, self).start()
@@ -55,6 +63,16 @@ class MCPMasterHandler(BaseApp):
                 await connection.stop_serve()
                 del connection
 
+        # make sure there is not connection check task
+        coros = []
+        connection_check_task = self.connection_check_task
+        self.connection_check_task.clear()
+
+        for task in connection_check_task:
+            coros.append(task.cancel())
+
+        await asyncio.gather(*coros)
+
     @observe_event(mcp_event.EventMCPStateChange, MC_DISCONNECT)
     def disconnecting_handler(self, ev: event.EventSocketConnecting):
         conn = ev.connection
@@ -79,7 +97,56 @@ class MCPMasterHandler(BaseApp):
         self.connection_dict[conn.id] = conn
         self.ip_to_connection[conn.address[0]] = conn
 
+        if conn.address[0] in self.connection_event:
+            self.connection_event[conn.address[0]].set()
+
     @observe_event(ReqGetAgentConnection)
     def get_agent_connection_handler(self, ev: ReqGetAgentConnection):
         result = self.ip_to_connection.get(ev.agent, None) or AgentIsNotExist(ev.agent)
         ev.push_reply(result)
+
+    async def check_connection(self, ev: ReqWaitAgentsConnect):
+        agents:list = ev.agents
+        ip_to_connection = self.ip_to_connection
+
+        remaining_agents = []
+
+        event_dict = {}
+        wait_coros = []
+
+        for agent in agents:
+            # check the target agent exist or not.
+            if agent in ip_to_connection:
+                continue
+
+            # create task to wait conenction event been set.
+            remaining_agents.append(agent)
+            conn_event: asyncio.Event = self.connection_event[agent]
+            if conn_event.is_set():
+                conn_event.clear()
+            event_dict[agent] = conn_event
+            wait_coros.append(conn_event.wait())
+
+        if len(remaining_agents) == 0:
+            ev.push_reply(True)
+            return
+
+        try:
+            gather_tasks = asyncio.gather(*wait_coros)
+            await asyncio.wait_for(gather_tasks, timeout=ev.wait_connection_timeout)
+            ev.push_reply(True)
+        except asyncio.TimeoutError:
+            remaining_agents.clear()
+
+            for agent, conn_event in event_dict.items():
+                if conn_event.is_set():
+                    continue
+                remaining_agents.append(agent)
+
+            ev.push_reply(Exception(f"Agent not connect, {remaining_agents}"))
+
+        self.connection_check_task.remove(asyncio.current_task())
+
+    @observe_event(ReqWaitAgentsConnect)
+    def wait_agents_handler(self, ev: ReqWaitAgentsConnect):
+        self.connection_check_task.add(spawn(self.check_connection, ev))
--- async_app_fw/controller/mcp_controller/master_lib/event.py
+++ async_app_fw/controller/mcp_controller/master_lib/event.py
@@ -8,3 +8,12 @@ class ReqGetAgentConnection(EventAsyncRequestBase):
     def __init__(self, agent, timeout=5):
         super().__init__(timeout)
         self.agent = agent
+
+class ReqWaitAgentsConnect(EventAsyncRequestBase):
+    REQUEST_NAME = 'Requet, Check Agent Connect.'
+    DST_NAME = APP_NAME
+
+    def __init__(self, agents, wait_connection_timeout=10, timeout=None):
+        super().__init__(timeout=None)
+        self.wait_connection_timeout = wait_connection_timeout
+        self.agents = agents
